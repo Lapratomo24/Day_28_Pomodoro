d42f267970890cf4afa796ced9d40acfb88ad2b9
Aditya <lapratomo24@yahoo.com>
2024-09-22 14:52:00 +0200

    added logic to different time sessions

--- main.py
+++ main.py
@@ -10,13 +10,29 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+REPS = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(300)
+    global REPS
+    REPS += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if REPS % 8 == 0:
+        count_down(long_break_sec)
+        label.config(text="Long Break", fg=RED)
+    if REPS % 2 == 0:
+        count_down(short_break_sec)
+        label.config(text="Short Break", fg=RED)
+    else:
+        count_down(work_sec)
+        label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -29,6 +45,8 @@ def count_down(count):
     canvas.itemconfig(timer, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -36,7 +54,7 @@ window = Tk()
 window.title("Pomodoro Technique")
 window.config(padx=120, pady=100, bg=YELLOW)
 
-label = Label(text="Pomodoro Timer", font=("Courier", 20, "bold"), fg=RED, bg=YELLOW)
+label = Label(text="Pomodoro Timer", font=("Courier", 25, "bold"), fg=RED, bg=YELLOW)
 label.grid(column=1, row=0)
 
 btn1 = Button(text="Start", bg=GREEN, font=("Courier", 12, "normal"), command=start_timer)
@@ -45,8 +63,8 @@ btn1.grid(column=0, row=2)
 btn2 = Button(text="Reset", bg=GREEN, font=("Courier", 12, "normal"))
 btn2.grid(column=2, row=2)
 
-label = Label(text="✅", font=("Courier", 15, "bold"), bg=YELLOW, fg=GREEN)
-label.grid(column=1, row=3)
+check = Label(text="✅", font=("Courier", 15, "bold"), bg=YELLOW, fg=GREEN)
+check.grid(column=1, row=3)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 img = PhotoImage(file="tomato.png")
