c3588be3a4a1ed2ceed975f4cc480d547839aef8
Aditya <lapratomo24@yahoo.com>
2024-09-22 11:00:33 +0200

    created UI using canvas

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Technique")
+window.config(padx=100, pady=50, bg=YELLOW)
 
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+canvas.pack()
 
 window.mainloop()
